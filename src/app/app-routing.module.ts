2c11133311613ce36808b310c1effed5f6f28676
lacnhiau <nhial20406@st.uel.edu.vn>
2023-04-06 20:59:31 +0700

    #nhi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
 import { HompageComponent } from './hompage/hompage.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
-  {path:"footer",component:FooterComponent},
   {path:"contact",component:ContactComponent},
   {path:"sign-up",component:SignUpComponent},
   {path:"",component:HompageComponent},
-
 ];
 
 @NgModule({
@@ -19,7 +16,6 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 export const RoutingComponent={
-  FooterComponent,
   ContactComponent,
   HompageComponent,
   SignUpComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
 import { HompageComponent } from './hompage/hompage.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -11,7 +10,6 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     ContactComponent,
     HompageComponent,
     SignUpComponent
