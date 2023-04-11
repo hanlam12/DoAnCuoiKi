85dcd759c8d016a7a69d1d20e0df31016759df03
LTS <sonlt406k20@gmail.com>
2023-04-12 00:39:16 +0700

    LTS

--- src/app/congty/congty.component.ts
+++ src/app/congty/congty.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-congty',
   templateUrl: './congty.component.html',
   styleUrls: ['./congty.component.css']
 })
-export class CongtyComponent {
+export class CongtyComponent implements AfterViewInit {
+  private map: L.Map | undefined;
 
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map-container').setView([51.505, -0.09], 13);
+    // add tile layer, markers, etc.
+  }
 }
