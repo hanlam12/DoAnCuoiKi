6bb820811e050f2aa8e8b7cef6a6c901e48fa2d3
LTS <sonlt406k20@gmail.com>
2023-04-13 19:49:58 +0700

    LTS Done FE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
@@ -11,6 +10,9 @@ import { ThongtinComponent } from './thongtin/thongtin.component';
 import { LoginComponent } from './login/login.component';
 import { DieukhoandichvuComponent } from './dieukhoandichvu/dieukhoandichvu.component';
 import { CongtyComponent } from './congty/congty.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 
@@ -30,6 +32,8 @@ import { CongtyComponent } from './congty/congty.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
     
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +7,31 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  public lgForm: FormGroup = new FormGroup({
+    
+  });
+  password = ''
+  showPassword =""
 
+  constructor(private lg: FormBuilder) {
+    this.lgForm = this.lg.group({
+      email: [''],
+      password: [''],
+      checkbox: ['false']
+    });
+  }
+
+  error = false
   
+  
+  onSubmit() {
+   if (this.lgForm.get('email')?.value == '' || this.lgForm.get('password')?.value == '' || this.lgForm.get('email')?.value.indexOf('@') == -1  ) {
+    this.error = true
+   }
+   else {
+    console.log(this.lgForm.value);
+    this.error = false
+   }
+    
+  }
 }
