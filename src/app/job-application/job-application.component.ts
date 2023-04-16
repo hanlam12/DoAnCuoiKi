4c2ba6a69f4bda37e68dcdf69e08c820e635a3af
ThuHang2408 <hangnt20406@st.uel.edu.vn>
2023-04-16 18:11:09 +0700

    Hanghee

--- src/app/job-application/job-application.component.ts
+++ src/app/job-application/job-application.component.ts
@@ -7,12 +7,46 @@ import { Component } from '@angular/core';
 })
 export class JobApplicationComponent {
   isOn: boolean = true;
-  showCv = false
 
-  toggle(){
-    this.showCv = !this.showCv
-  }
   status() {
     this.isOn = !this.isOn;
   }
+  hoSoDaTao = true
+  taiCv = true
+  showCv = false
+  UpdateCv = false
+
+
+  toggle(){
+    this.hoSoDaTao = false,
+    this.taiCv = false;
+    this.showCv = true;
+
+  }
+  updateCv(){
+    this.taiCv = false;
+    this.UpdateCv=true;
+    this.showCv = false;
+    this.hoSoDaTao = false
+}
+cancel(){
+  this.taiCv = true;
+  this.UpdateCv=false;
+  this.showCv = false;
+  this.hoSoDaTao = true
+}
+
+
+// handleFileInput(event) {
+//   const file = event.target.files[0]; // Lấy ra file đầu tiên được chọn
+//   const reader = new FileReader();
+//   reader.readAsDataURL(file); // Đọc file dưới dạng base64
+
+//   reader.onload = () => {
+//     const base64String = reader.result as string;
+//     // Gửi file lên server bằng một HTTP request ở đây
+//   };
+// }
+
+
 }
